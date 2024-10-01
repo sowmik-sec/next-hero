83051ed99f945ab09a9177687691c5d4b5306241
sowmik-sec <sowmik.sec@gmail.com>
2024-10-01 20:50:01 +0600

    Get NextAuth Session Data In Client and Server Components

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,36 @@
-const { default: NextAuth } = require("next-auth");
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   session: {
     strategy: "jwt",
   },
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          required: true,
+          placeholder: "Enter email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          required: true,
+          placeholder: "Enter password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        } else {
+          return true;
+        }
+      },
+    }),
+  ],
 });
 
-export { handler as Get, handler as POST };
+export { handler as GET, handler as POST };
